d33a055ee6a58cf0802721d679d3e208a47cedd7
Liene <81748500+LucyferYX@users.noreply.github.com>
2024-03-04 13:05:16 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -14,23 +14,36 @@ namespace CMP1903_A1_2324 {
             // Creates a Testing object to verify the output and operation of the other classes
             Testing tester = new Testing();
 
-            // Outputs instructions to console
             bool doRoll = true;
-            Console.WriteLine("Press 1 for Die, 2 for Game and any other key to exit.");
+            int sumOfDie = 0;
+            int sumOfGame = 0;
+
+            // Outputs instructions to console
+            Console.WriteLine("Press 1 for Die, 2 for Game, 3 for die sum of Die, 4 for die sum of Game and any other key to exit.");
 
             // Continuous dice rolls
             while (doRoll) {
                 // Reads user's pressed key
                 ConsoleKeyInfo keyInfo = Console.ReadKey(intercept: true);
 
-                // Rolls die for input 1
                 if (keyInfo.Key == ConsoleKey.D1) {
-                    Console.WriteLine($"Number of die is {die.DieRoll()}.");
-                    // Outputs dice sum for input 2
+                    // Outputs Die roll for input 1
+                    int dieRoll = die.DieRoll();
+                    sumOfDie += dieRoll;
+                    Console.WriteLine($"Number of die is {dieRoll}.");
                 } else if (keyInfo.Key == ConsoleKey.D2) {
-                    Console.WriteLine($"Sum of 3 die rolls is {game.RollAndSum()}.");
-                    // Exits for any other input
+                    // Outputs Game die sum for input 2
+                    int gameRoll = game.RollAndSum();
+                    sumOfGame += gameRoll;
+                    Console.WriteLine($"Sum of 3 die rolls is {gameRoll}.");
+                } else if (keyInfo.Key == ConsoleKey.D3) {
+                    // Outputs all Die roll sum for input 3
+                    Console.WriteLine($"Total die sum for Die is {sumOfDie}.");
+                } else if (keyInfo.Key == ConsoleKey.D4) {
+                    // Outputs all Game roll sum for input 4
+                    Console.WriteLine($"Total die sum for Game is {sumOfGame}.");
                 } else {
+                    // Exits for any other input
                     doRoll = false;
                 }
 
